06b6eb0785242eac35cb0ef3365597fc8c16a197
imuraki <akhilkc9@gmail.com>
2019-12-25 00:28:39 -0500

    Updates

---
+++ precache-manifest.223a9755c58c9335455356c6c5ed81ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7c1bfc0e4edede3884d",
+    "url": "/my-portfolio/static/css/main.53ab8d35.chunk.css"
+  },
+  {
+    "revision": "a7c1bfc0e4edede3884d",
+    "url": "/my-portfolio/static/js/main.a7c1bfc0.chunk.js"
+  },
+  {
+    "revision": "d4d95a87c7a02a69aacb",
+    "url": "/my-portfolio/static/js/runtime~main.d4d95a87.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/my-portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "e0f7c47545f5b63883f2c6512e89ee02",
+    "url": "/my-portfolio/index.html"
+  }
+];
